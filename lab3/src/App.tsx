5809495871e012d6db99cf279b840937f8c11a74
CrycifyMe <125039730+crycifyme@users.noreply.github.com>
2023-06-08 15:42:54 +0300

    Add files via upload

--- lab3/src/App.tsx
+++ lab3/src/App.tsx
@@ -1,75 +1,174 @@
-import React, { useState } from 'react';
-import { Layout, Menu, Card, Form, Input, Button } from 'antd';
-const { SubMenu } = Menu;
+import React, { useState, useEffect } from 'react';
+import { useStore } from './MyStore';
+import './index.css';
 
-const { Header, Content, Footer, Sider } = Layout;
+function MyComponent() {
+    const store = useStore();
 
-const App = () => {
-    const [form] = Form.useForm();
-    const [cards, setCards] = useState([
-        { title: 'Laboratorul 1', content: 'Prezentat', prenume: 'Nota 10' },
+    const [localData, setLocalData] = useState<any>({ name: '', address: '', phone: '' });
+    const myData = store.myData;
 
-    ]);
-    const handleSubmit = (values: { title: string; content: string; prenume: string }) => {
-        const newCard = { title: values.title, content: values.content, prenume: values.prenume };
-        const newCards = [...cards, newCard];
-        setCards(newCards);
-        console.log('valorile', values);
-        alert('Forma a fost adaugata');
+    const [isEditing, setIsEditing] = useState(false);
+    const [editingIndex, setEditingIndex] = useState(-1);
+
+    const handleAddObject = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        if (!localData.name || !localData.address || !localData.phone) {
+            alert('Introduceti toate campurile.');
+            return;
+        }
+
+        setIsLoading(true);
+        setTimeout(() => {
+            store.addObject(localData);
+            localStorage.setItem('myData', JSON.stringify(store.myData));
+            setIsLoading(false);
+        }, 2000);
+
     };
+
+
+    const handleDeleteObject = (index: number) => {
+        store.deleteObject(index);
+        setLocalData({ name: '', address: '', phone: '' });
+        localStorage.setItem('myData', JSON.stringify(store.myData));
+    };
+
+    const handleUpdateObject = (index: number, newData: any) => {
+        store.updateObject(index, newData);
+        setIsLoading(true);
+        setTimeout(() => {
+            localStorage.setItem('myData', JSON.stringify(store.myData));
+            setIsLoading(false);
+        }, 2000);
+
+        // setează valorile din obiectul myData în starea localData
+        const dataToUpdate = store.myData[index];
+        setLocalData({ name: dataToUpdate.name, address: dataToUpdate.address, phone: dataToUpdate.phone });
+        setIsEditing(false);
+        setEditingIndex(-1);
+    };
+
+    const handleEditObject = (index: number) => {
+        const dataToUpdate = store.myData[index];
+        setLocalData({ name: dataToUpdate.name, address: dataToUpdate.address, phone: dataToUpdate.phone });
+        setIsEditing(true);
+        setEditingIndex(index);
+    };
+
+    const handleSaveObject = () => {
+        if (editingIndex >= 0) {
+            handleUpdateObject(editingIndex, localData);
+        }
+    };
+
+    const [isLoading, setIsLoading] = useState(false);
+
+    useEffect(() => {
+        const data = localStorage.getItem('myData');
+        if (data) {
+            store.setMyData(JSON.parse(data));
+        } else {
+            localStorage.setItem('myData', JSON.stringify(store.myData));
+        }
+    }, [store]);
+
+    useEffect(() => {
+        if (!isLoading) {
+            const data = localStorage.getItem('myData');
+            if (data) {
+                store.setMyData(JSON.parse(data));
+            }
+        }
+    }, [isLoading, store]);
+
     return (
-        <Layout>
-            <Header style={{ display: 'flex', justifyContent: 'center' }}>
-                <Menu theme="dark" mode="horizontal">
-                    <Menu.Item key="1">LABORATORUL 3</Menu.Item>
-                </Menu>
-            </Header>
-            <Layout>
-                <Sider width={300} style={{ background: '#fff' }}>
-                    <Menu
-                        theme="light"
-                        mode="inline"
-                        defaultSelectedKeys={['1']}
-                        defaultOpenKeys={['sub1']}
-                        style={{ height: '100%', borderRight: 0 }}
-                    >
-                        <SubMenu key="sub1" title="Menu">
-                            <Menu.Item key="1">Laboratoarele necesare</Menu.Item>
-                            <Menu.Item key="2">Laboratoarele date</Menu.Item>
-                            <Menu.Item key="3">Laboratoarele in proces</Menu.Item>
-                        </SubMenu>
-                    </Menu>
-                </Sider>
 
-                <Content style={{ padding: '50px' }}>
-                    <Form form={form} onFinish={handleSubmit}>
-                        <Form.Item name="title" rules={[{ required: true, message: 'Introduceți Laboratorul' }]}>
-                            <Input placeholder="Laboratorul" />
-                        </Form.Item>
-                        <Form.Item name="content" rules={[{ required: true, message: 'Introduceti starea' }]}>
-                            <Input placeholder="Prezentat sau ne prezentat" />
-                        </Form.Item>
-                        <Form.Item name="prenume" rules={[{ required: true, message: 'Introduceti nota' }]}>
-                            <Input placeholder="Nota" />
-                        </Form.Item>
-                        <Form.Item>
-                            <Button type="primary" htmlType="submit">
-                                Trimite
-                            </Button>
-                        </Form.Item>
-                    </Form>
-                    <div style={{ display: 'flex', flexWrap: 'wrap' }}>
-                        {cards.map(card => (
-                            <Card key={card.title} style={{ width: 300}}>
-                                <Card.Meta title={`${card.title} - ${card.prenume}`} description={card.content} />
-                            </Card>
-                        ))}
-                    </div>
-                </Content>
-            </Layout>
+        <div className="container">
+            <h1 className="title">Varzari Nicolae Laboratorul 5.2</h1>
+            <header>
+                <img
+                    src="https://www.freepnglogos.com/uploads/mr-bean/rowan-atkinson-png-download-bean-png-image-for-0.png"
+                    alt="logo" className="logo"/>
+            </header>
+            <form className="form" onSubmit={handleAddObject}>
+                <input
+                    className="input"  type="text" placeholder="Nume" defaultValue={localData.name} onChange={(e) => setLocalData({ ...localData, name: e.target.value })}
+                />
+                <input
+                    className="input" type="text" placeholder="Adresa" defaultValue={localData.address} onChange={(e) => setLocalData({ ...localData, address: e.target.value })}
+                />
+                <input
+                    className="input" type="text" placeholder="Telefon" defaultValue={localData.phone} onChange={(e) => setLocalData({ ...localData, phone: e.target.value })}
+                />
+                <button className="button" type="submit">{isLoading ? 'Se incarca...' : 'Adauga'}</button>
 
-        </Layout>
+            </form>
+            <table className="table">
+                <thead>
+                <tr>
+                    <th>Nume</th>
+                    <th>Adresa</th>
+                    <th>Telefon</th>
+                    <th>Actiuni</th>
+                </tr>
+                </thead>
+                <tbody>
+                {myData.map((item: any, index: number) => (
+                    <tr key={index}>
+                        <td>{item.name}</td>
+                        <td>{item.address}</td>
+                        <td>{item.phone}</td>
+                        <td>
+                            <button className="button edit" onClick={() => handleEditObject(index)}>
+                                Editeaza
+                            </button>
+                            <button className="button delete" onClick={() => handleDeleteObject(index)}>
+                                Sterge
+                            </button>
+                        </td>
+                    </tr>
+                ))}
+                </tbody>
+            </table>
+            {isEditing && (
+                <div className="edit-form">
+                    <h2>Editare</h2>
+                    <form onSubmit={handleSaveObject}>
+                        <input
+                            className="input"
+                            type="text"
+                            placeholder="Nume"
+                            value={localData.name}
+                            onChange={(e) => setLocalData({ ...localData, name: e.target.value })}
+                        />
+                        <input
+                            className="input"
+                            type="text"
+                            placeholder="Adresa"
+                            value={localData.address}
+                            onChange={(e) => setLocalData({ ...localData, address: e.target.value })}
+                        />
+                        <input
+                            className="input"
+                            type="text"
+                            placeholder="Telefon"
+                            value={localData.phone}
+                            onChange={(e) => setLocalData({ ...localData, phone: e.target.value })}
+                        />
+                        <div className="edit-form-buttons">
+                            <button className="button save" type="submit">
+                                Salveaza
+                            </button>
+                            <button className="button cancel" onClick={() => setIsEditing(false)}>
+                                Anuleaza
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            )}
+        </div>
     );
-};
+}
 
-export default App;
+export default MyComponent;
